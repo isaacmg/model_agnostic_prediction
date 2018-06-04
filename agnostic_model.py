9ac1bb8f078dd87aee4b9b3c5d12f47aa2ae0773
isaacmg <igodfried@isaac26.com>
2018-06-04 00:56:26 -0400

    Readme and updates to class
    Class updates and readme

--- agnostic_model.py
+++ agnostic_model.py
@@ -1,19 +1,25 @@
 
 class ModelAgnostic(object):
-    def __init__(self, container_url=None, predict_function, init_model, weight_path):
+    def __init__(self, container_url=None, predict_function, init_model, import_type="inference", weight_path):
         self.container_url = container_url
         if not container_url:
             self.weight_path = weight_path
-            self.model = init_model(weight_path)
+            self.model = init_model(weight_path, import_type)
         self.predict_function = preprocess_function
         self.result = None
+        self.import_type = import_type
+
+    def init_model(self, model_type="inference"):
+        if model_type is "inference":
+            res = infer_model_type(self.weight_path)
+            loader = __import__(res, fromlist=[''])
+            return
+        else:
 
 
-    def init_model(self, model_type="infer"):
 
     def infer_model_type(self):
         # TODO infers the model given the weight extension
-        extension_to_model_type = {} 
-        self.weight_path.split('.')[1]
-
-        return
+        extension_to_model_type = {".h5":"keras", ".pth":"pytorch", ".pt":"torch" ".pb":"tensorflow"}
+        result = extension_to_model_type[self.weight_path.split('.')[1]]
+        return result
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Agnostic model loader
+
+
+This provides any easy model instantiation for Flask, Django, and other production Python projects.
+It allows you to easily load models and perform predictions without having to worry about the different types.
