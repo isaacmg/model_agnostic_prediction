c9f9d579600bb2c4214438b66c74586a90e6c622
isaacmg <igodfried@isaac26.com>
2018-06-04 12:58:36 -0400

    created model factory class and preprocessing

--- agnostic_model.py
+++ agnostic_model.py
@@ -1,4 +1,17 @@
-
+class ModelFactory(object):
+    def infer_model_type(self):
+        # TODO infers the model given the weight extension
+        extension_to_model_type = {".h5":"keras", ".pth":"torch", ".pt":"torch" ".pb":"tensorflow"}
+        result = extension_to_model_type[self.weight_path.split('.')[1]]
+        return result
+    def init_model(self, model_type="inference"):
+        if model_type is "inference":
+            model_type = infer_model_type(self.weight_path)
+        loader = __import__(model_type, fromlist=[''])
+        # TODO need to dynamicall load model given the different weights.
+        model = loader.save()
+        return model 
+    
 class ModelAgnostic(object):
     def __init__(self, container_url=None, predict_function, init_model, import_type="inference", weight_path):
         self.container_url = container_url
@@ -8,20 +21,6 @@ class ModelAgnostic(object):
         self.predict_function = preprocess_function
         self.result = None
         self.import_type = import_type
-
-    def init_model(self, model_type="inference"):
-        if model_type is "inference":
-            model_type = infer_model_type(self.weight_path)
-        loader = __import__(model_type, fromlist=[''])
-        # TODO need to dynamicall load model given the different weights.
-        model = loader.save()
-        return model 
-    
-    def infer_model_type(self):
-        # TODO infers the model given the weight extension
-        extension_to_model_type = {".h5":"keras", ".pth":"torch", ".pt":"torch" ".pb":"tensorflow"}
-        result = extension_to_model_type[self.weight_path.split('.')[1]]
-        return result
     
     def preprocessing(self, items):
         """
