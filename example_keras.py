3ea53852d9782dd64f3b74e8117f8875812a6a10
isaacmg <igodfried@isaac26.com>
2018-06-05 20:14:49 -0400

    Added to example_keras

--- example_keras.py
+++ example_keras.py
@@ -16,3 +16,20 @@ class ChexNet(KerasModel):
 class SimpleResNet50(KerasModel):
     def __init__():
         super(SimpleResNet50, self).__init__(self, "keras", weight_path)
+        from keras.applications.resnet50 import preprocess_input
+
+    def create_model(self):
+         model = keras.applications.resnet50.ResNet50
+        (include_top=True, weights='imagenet', input_tensor=None,
+        input_shape=None, pooling=None, classes=1000)
+        return model
+
+    def preprocessing(self, image_path):
+        img = image.load_img(image_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        return x
+
+    def process_result(self):
+        return self.result 
