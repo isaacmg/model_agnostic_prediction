7233c2e252510309c5d4b282972fcc7b655d5e78
isaacmg <igodfried@isaac26.com>
2018-06-11 00:35:59 -0400

    updated model

--- agnostic_model.py
+++ agnostic_model.py
@@ -74,6 +74,7 @@ class PytorchModel(ModelAgnostic):
         self.torch = __import__('torch')
         super().__init__(weight_path, "PyTorch")
         self.model = self.create_model()
+        self.model = self.model.load_state_dict('weight')
 
     def create_model(self):
         pass 
