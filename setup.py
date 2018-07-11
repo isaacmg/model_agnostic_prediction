c5e3eb20fb82d13c9173880f70f5b55224ded729
isaacmg <igodfried@isaac26.com>
2018-07-11 15:22:43 -0400

    Updated readme to travis

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.1dev',
     packages=['model_agnostic', 'model_agnostic.models', 'model_agnostic.models.chexnet_files'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
+
 )
