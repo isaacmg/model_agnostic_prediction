f8076fd666b9bf1c4f2221663e5c225e33dc1e31
isaacmg <igodfried@isaac26.com>
2018-07-11 15:36:05 -0400

    Fixed name problems (builds fine) here

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
     name='model_agnostic',
     version='0.1dev',
-    packages=['model_agnostic', 'model_agnostic.models', 'model_agnostic.models.chexnet_files'],
+    packages=['model_agnostic', 'model_agnostic.models', 'model_agnostic.example_models', 'model_agnostic.example_models.chexnet_files'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='A package to easily serve machine learning models',
 
 )
