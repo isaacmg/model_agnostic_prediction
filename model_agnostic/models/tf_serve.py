a327a876207f68666bb7501a3c5c52c01d31e83c
isaacmg <igodfried@isaac26.com>
2018-08-13 23:08:23 -0400

    changed version number for r
    Changed the version prepending release 0.3.

--- model_agnostic/models/tf_serve.py
+++ model_agnostic/models/tf_serve.py
@@ -5,8 +5,9 @@ import tensorflow as tf
 
 class TensorflowModel(ModelAgnostic):
     def __init__(self, weight_path, load_type, backend="tensorflow"):
-        # TODO IMPLEMENT
-        self.tf = "s"
+        self.graph = tf.Graph() 
+        self.session = tf.Session()
+        self.saver = tf.train.Saver()
         
     def create_model(self, weight_path):
         """
@@ -14,10 +15,18 @@ class TensorflowModel(ModelAgnostic):
         Implement this function if you saved just the model weights and
         not the architecture.
         """
-        pass    
+        self.saver.restore(self.session, tf.train.load_checkpoint(weight_path))
+        
+
+        
 
     def preprocessing(self, items):
         pass
 
     def predict(self, formatted_data, batch_size=None):
         self.model.run()
+
+    def export_tf_serving(self):
+        pass 
+
+        
