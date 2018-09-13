2faf9bc7baed1db76f3736b1e2505e1aab8201f4
isaacmg <igodfried@isaac26.com>
2018-09-12 22:42:13 -0400

    graph pip and tf

---
+++ model_agnostic/models/graph_pipe.py
@@ -0,0 +1,19 @@
+import sys 
+sys.path.append("..")
+from agnostic_model import ModelAgnostic
+from graphpipe import remote
+
+class GraphPipeRemote(ModelAgnostic):
+    def __init__(self, url):
+      self.graph_pipe_url = url
+    
+    def preprocessing(self, items):
+        pass
+
+    def predict(self, formatted_data, batch_size=None):
+        y = remote.execute(self.graph_pipe_url, formatted_data)
+
+    def process_result(self, result_data):
+        pass
+
+        
